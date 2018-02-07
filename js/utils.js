89f357bb73b87a56c6090fca59b49b33f2def424
zuluoaaa <864457005@qq.com>
2018-02-07 16:18:34 +0800

    just start

---
+++ js/utils.js
@@ -0,0 +1,40 @@
+function createShader(gl,type,source){
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader,source);
+    gl.compileShader(shader);
+
+    var success = gl.getShaderParameter(shader,gl.COMPILE_STATUS);
+    if(success){
+        return shader;
+    }
+    console.log(gl.getShaderInfoLog(shader))
+
+    gl.deleteShader(shader);
+}
+
+function createProgram(gl,vertexShader,fragmentShader) {
+    console.log(vertexShader,fragmentShader)
+    var program = gl.createProgram();
+    gl.attachShader(program,vertexShader);
+    gl.attachShader(program,fragmentShader);
+    gl.linkProgram(program);
+
+    var success = gl.getProgramParameter(program,gl.LINK_STATUS);
+    if(success){
+        return program;
+    }
+    new Error(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+}
+
+function resizeCanvasToDisplaySize(canvas,multiplier) {
+    multiplier = multiplier || 1;
+    var width = canvas.clientWidth * multiplier | 0;
+    var height = canvas.clientHeight * multiplier | 0;
+    if(canvas.width !== width || canvas.height !== height){
+        canvas.width = width;
+        canvas.height = height;
+        return true;
+    }
+    return false;
+}
